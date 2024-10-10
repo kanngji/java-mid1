d0cc19610ee8cb388a72247ef4bc32b31c1c6685
kanngji <alleneffort1994@gmail.com>
2024-10-11 01:21:44 +0900

    불변객체

---
+++ src/lang/immutable/address/PrimitiveMain.java
@@ -0,0 +1,23 @@
+package lang.immutable.address;
+
+public class PrimitiveMain {
+    public static void main(String[] args) {
+        // ㄱㅣ본형은 절대로 같은 값을 공유허지않는다
+        int a = 10;
+        int b = a; // a -> b 값 복사 후 대입
+
+        System.out.println("a= "+ a);
+        System.out.println("b= "+ b);
+
+        b =20;
+        System.out.println("20 => b ");
+        System.out.println("a= "+ a);
+        System.out.println("b= "+ b);
+
+        System.out.println(a+b);
+    }
+
+
+
+
+}
