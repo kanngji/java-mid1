ce8844e9ef8d2d06fc8bee756f48a64ff07dde84
kanngji <alleneffort1994@gmail.com>
2024-10-15 00:04:50 +0900

    String

---
+++ src/lang/string/StringBasicMain.java
@@ -0,0 +1,13 @@
+package lang.string;
+
+public class StringBasicMain {
+    public static void main(String[] args) {
+
+        //int , double , boolean
+        String str1 = "hello";
+        String str2 = new String("gogogo");
+
+        System.out.println("str1"+str1);
+        System.out.println("str2"+str2);
+    }
+}
