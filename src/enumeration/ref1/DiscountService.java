e6eff9b4b38858ffd0c60efc168e8dccd13b5e48
kanngji <alleneffort1994@gmail.com>
2024-11-14 01:12:25 +0900

    열거형- 리팩토링1

---
+++ src/enumeration/ref1/DiscountService.java
@@ -0,0 +1,13 @@
+package enumeration.ref1;
+
+
+
+
+public class DiscountService {
+    public int discount(ClassGrade classGrade, int price) {
+        int discountPercent = classGrade.getDiscountPercent();
+
+
+        return price * discountPercent / 100;
+    }
+}
